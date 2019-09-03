79c886fe12a9616e382418dbb23f6b0b7668d206
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-03 02:48:21 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,11 @@
 class Project 
   
   attr_accessor :title
-  attr_reader :projects
+  attr_reader :backers
   
   def initialize(title)
     @title = title
-    @bakers = []
+    @backers = []
   end
   
   def add_backer(backer)
