0e087e682d58fbbae78a87c1ea964a93fd413e27
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-03 02:35:38 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,12 @@
 class Backer
   
+  attr_accessor :name, :backed_projects
   attr_reader :backed_projects
   
-  def initialize(backed_projects)
+  def initialize(name)
+    @name = name
     @backed_projects = []
-    
   end
+  
+  
 end
